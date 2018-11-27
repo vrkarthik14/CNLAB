9e637763fc4e15e836a43c7d947c28e9999e58fa
V R KARTHIK <vrkarthik14@gmail.com>
2018-11-27 08:51:06 +0530

    Update server.c

--- ex1a/server.c
+++ ex1a/server.c
@@ -1,3 +1,7 @@
+/*Socket programming is a way of connecting two nodes on a network to communicate with each other.
+One socket(node) listens on a particular port at an IP, while other socket reaches out to the other
+to form a connection. Server forms the listener socket while client reaches out to the server.*/
+
 //implement real time analytic, instant messaging, binary streaming, and document collaboration. 
 
 #include <stdio.h>
